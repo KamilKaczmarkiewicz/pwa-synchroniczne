d3d1802115ea7812c2346573ec3dd0569859e978
Dawid Bruski <dawidbruski1@gmail.com>
2024-04-04 22:48:59 +0200

    added promos and images above footer

---
+++ components/MainPage/PromoSection/index.vue
@@ -0,0 +1,42 @@
+<script>
+const promos = [
+  { title: "outlet", description: 'Wyprzedaz 2023', imgPath: "promo1.png", ctaText: "zobacz" },
+  { title: "kolekcja 2020", description: 'Nowa kolekcja', imgPath: "promo2.png", ctaText: "zobacz" },
+]
+import PromoItem from './PromoItem.vue'
+
+export default {
+  data() {
+    return {
+      promos,
+    }
+  },
+  components: {
+    PromoItem,
+  }
+}
+</script>
+<template>
+  <div style="width: 100%;flex:1;">
+  <h2 class="header">Promocje</h2>
+  <div class="promoContainer">
+    <PromoItem v-bind:promoItem="promoItem" v-for="promoItem in promos" />
+  </div>
+  </div>
+</template>
+<style scoped>
+  .promoContainer {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 8px;
+    gap: 16px;
+  }
+  .header {
+    text-align: center;
+    margin: 1em 0;
+    font-size: 2em;
+    font-weight: 700;
+    color: #000;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
 <template>
     <h3 style="min-height: 100vh;">index</h3>
+    <MainPagePromoSection/>
+    <MainPageTilesSection />
 </template>
+
+<script>
+import MainPageTilesSection from '../components/MainPage/TilesSection'
+import MainPagePromoSection from '../components/MainPage/PromoSection'
+export default {
+  components: {
+    MainPageTilesSection,
+    MainPagePromoSection,
+  }
+}
+</script>
