907a6a7e46882eed83a7da5a9050fd4b74eabe7e
Dawid Bruski <dawidbruski1@gmail.com>
2024-04-04 23:15:04 +0200

    added another section

---
+++ components/MainPage/InfoSection/InfoItem.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  const props = defineProps(['infoItem'])
+</script>
+
+<template>
+  <div class="infoContainer">
+    <i-discount v-if="infoItem.icon === 'discount'" class="icon"/>
+    <i-privacy v-else-if="infoItem.icon === 'privacy'" class="icon"/>
+    <i-shoppingbag v-else-if="infoItem.icon === 'shoppingbag'" class="icon"/>
+    <p class="infoTitle">{{ infoItem.title }}</p>
+    <p>{{ infoItem.description }}</p>
+  </div>
+</template>
+
+<style scoped>
+  .infoContainer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+  .infoTitle {
+    margin: 1em 0;
+  }
+  .icon {
+    font-size: 72px;
+    color: #000;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
     <h3 style="min-height: 100vh;">index</h3>
+    <MainPageInfoSection/>
     <MainPagePromoSection/>
     <MainPageTilesSection />
 </template>
 
 <script>
+import MainPageInfoSection from '../components/MainPage/InfoSection'
 import MainPageTilesSection from '../components/MainPage/TilesSection'
 import MainPagePromoSection from '../components/MainPage/PromoSection'
 export default {
   components: {
+    MainPageInfoSection,
     MainPageTilesSection,
     MainPagePromoSection,
   }
