0c7e6546ac4876c4d2eccb95f25d8900020f15af
Janke Bartosz <btqwow@gmail.com>
2024-04-13 20:14:23 +0200

    Dodanie bestsellerów

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,8 @@
 <template>
     <!-- <h3 style="min-height: 100vh;">index</h3> -->
-    <MainPageInfoSection/>
-    <MainPagePromoSection/>
+    <MainPageInfoSection />
+    <MainPagePromoSection />
+    <MainPageTopbestsellery />
     <MainPageTilesSection />
 </template>
 
-<script>
-import MainPageInfoSection from '../components/MainPage/InfoSection'
-import MainPageTilesSection from '../components/MainPage/TilesSection'
-import MainPagePromoSection from '../components/MainPage/PromoSection'
-export default {
-  components: {
-    MainPageInfoSection,
-    MainPageTilesSection,
-    MainPagePromoSection,
-  }
-}
-</script>
